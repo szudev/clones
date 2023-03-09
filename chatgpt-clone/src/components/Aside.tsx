28aa5ebe17572f98e61e32c8496aceca00b241d6
szudev <sz.ubiergo@gmail.com>
2023-03-09 15:01:03 -0300

    deleted test a tags in layout's aside component

--- chatgpt-clone/src/components/Aside.tsx
+++ chatgpt-clone/src/components/Aside.tsx
@@ -45,20 +45,6 @@ export default function Aside() {
             className='flex py-3 px-3 items-center gap-3 rounded-md hover:bg-gray-500/10 transition-colors duration-200 text-white cursor-pointer text-sm'
           >
             <LogOutIcon />
-            Whatever
-          </a>
-          <a
-            onClick={() => signOut()}
-            className='flex py-3 px-3 items-center gap-3 rounded-md hover:bg-gray-500/10 transition-colors duration-200 text-white cursor-pointer text-sm'
-          >
-            <LogOutIcon />
-            Whatever x2
-          </a>
-          <a
-            onClick={() => signOut()}
-            className='flex py-3 px-3 items-center gap-3 rounded-md hover:bg-gray-500/10 transition-colors duration-200 text-white cursor-pointer text-sm'
-          >
-            <LogOutIcon />
             Log out
           </a>
         </div>
