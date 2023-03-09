6403e979ba4376e272988f6330e8452069f203a9
szudev <sz.ubiergo@gmail.com>
2023-03-09 16:25:41 -0300

    fixed overflow-y on chat and sync it with the layout's sidebar, plus added custom utility classes in tailwind

--- chatgpt-clone/src/components/Chat.tsx
+++ chatgpt-clone/src/components/Chat.tsx
@@ -5,8 +5,8 @@ import ChatForm from './ChatForm'
 export default function Chat() {
   const messages = useMessageStore((state) => state.messages)
   return (
-    <div className='bg-gptgray w-full min-h-screen justify-between flex flex-col pt-[70px] md:pt-0'>
-      <main className='flex flex-col'>
+    <div className='bg-gptgray w-full h-screen justify-between flex flex-col pt-[70px] md:pt-0'>
+      <main className='flex flex-col overflow-y-auto'>
         {messages.map((entry) => (
           <Message key={entry.id} {...entry} />
         ))}
