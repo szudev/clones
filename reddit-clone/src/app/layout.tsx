b28f5008febc6bcd615c93b87cc9c7edfc4760db
szudev <sz.ubiergo@gmail.com>
2023-09-21 17:16:41 -0300

    added ts expected error in layout for navbar

--- reddit-clone/src/app/layout.tsx
+++ reddit-clone/src/app/layout.tsx
@@ -25,6 +25,7 @@ export default function RootLayout({
       )}
     >
       <body className='min-h-[100svh] pt-12 bg-slate-50 antialiased'>
+        {/* @ts-expect-error Server Component */}
         <Navbar />
         <div className='container max-w-7xl mx-auto h-full pt-12'>
           {children}
