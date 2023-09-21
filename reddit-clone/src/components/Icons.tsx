a4185ede7670a49473f83c36d179a66926292c6f
szudev <sz.ubiergo@gmail.com>
2023-09-21 17:09:36 -0300

    added navbar auth log in/out flow

--- reddit-clone/src/components/Icons.tsx
+++ reddit-clone/src/components/Icons.tsx
@@ -1,4 +1,4 @@
-import { LucideProps } from 'lucide-react'
+import { LucideProps, User } from 'lucide-react'
 
 export const Icons = {
   redditLogo: (props: LucideProps) => (
@@ -30,5 +30,6 @@ export const Icons = {
       />
       <path d='M1 1h22v22H1z' fill='none' />
     </svg>
-  )
+  ),
+  user: User
 }
--- reddit-clone/src/components/Navbar.tsx
+++ reddit-clone/src/components/Navbar.tsx
@@ -1,8 +1,11 @@
 import Link from 'next/link'
 import { Icons } from './Icons'
 import { buttonVariants } from './ui/Button'
+import { getAuthSession } from '@/lib/auth'
+import UserProfileNav from './UserProfileNav'
 
-export default function Navbar() {
+export default async function Navbar() {
+  const session = await getAuthSession()
   return (
     <div className='fixed top-0 inset-x-0 h-fit bg-zinc-100 border-b border-zinc-300 z-10 py-2'>
       <div className='container max-w-7xl h-full mx-auto flex items-center justify-between gap-2'>
@@ -12,9 +15,13 @@ export default function Navbar() {
             RedditClone
           </p>
         </Link>
-        <Link href='/sign-in' className={buttonVariants()}>
-          Sign In
-        </Link>
+        {session?.user ? (
+          <UserProfileNav user={session.user} />
+        ) : (
+          <Link href='/sign-in' className={buttonVariants()}>
+            Sign In
+          </Link>
+        )}
       </div>
     </div>
   )
