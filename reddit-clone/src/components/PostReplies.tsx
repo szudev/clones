1ab1a6e1e2b27c5798a3347526cb7903cf8d9e9b
szudev <sz.ubiergo@gmail.com>
2023-11-04 16:41:18 -0300

    replies fetching & loading fixed

--- reddit-clone/src/components/PostReplies.tsx
+++ reddit-clone/src/components/PostReplies.tsx
@@ -2,10 +2,10 @@
 
 import useFetchCommentReplies from '@/hooks/use-fetch-comment-replies'
 import { Button } from './ui/Button'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import PostReply from './PostReply'
 import { Session } from 'next-auth'
-import { Loader2 } from 'lucide-react'
+import { Loader2, ChevronDown, ChevronUp } from 'lucide-react'
 
 interface Props {
   repliesAmount: number
@@ -28,27 +28,37 @@ export default function PostReplies({
     isRefetching
   } = useFetchCommentReplies({ commentId })
   const [showReplies, setShowReplies] = useState<boolean>(false)
+  const [fetchReplies, setFetchReplies] = useState<boolean>(false)
 
   const handleShowReplies = () => {
     setShowReplies((prev) => {
-      if (prev === false) refetch()
+      if (!fetchReplies) setFetchReplies(true)
       return !prev
     })
   }
+
+  useEffect(() => {
+    if (fetchReplies) refetch()
+  }, [fetchReplies, refetch])
 
   const replies = data?.pages.flatMap((page) => page.replies)
   return (
     <section className='flex flex-col'>
       <Button
-        className='self-start'
+        className='self-start gap-2'
         variant='ghost'
         onClick={handleShowReplies}
       >
+        {showReplies ? (
+          <ChevronUp className='h-5 w-5 text-zinc-800' />
+        ) : (
+          <ChevronDown className='h-5 w-5 text-zinc-800' />
+        )}
         {repliesAmount === 1
           ? repliesAmount + ' reply'
           : repliesAmount + ' replies'}
       </Button>
-      {isRefetching && (
+      {(isRefetching || isFetching) && fetchReplies && !isFetchingNextPage && (
         <div className='flex h-full self-start ml-2 py-2 pl-4 items-center justify-center'>
           <Loader2 className='h-8 w-8 animate-spin text-zinc-500' />
         </div>
